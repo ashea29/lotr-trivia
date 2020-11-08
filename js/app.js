a145d260cff5d9fd2b719457891b4e2e11426049
ashea27 <shea.andrew27@gmail.com>
2020-11-08 12:43:54 -0700

    refactored html, styles, and game logic

--- js/app.js
+++ js/app.js
@@ -1,341 +1,265 @@
-//ASSIGNS ALL THE GAME QUESTIONS TO VARIABLES
-const question1 = "After Frodo was wounded on Weathertop, and Arwen takes him and begins heading to Rivendell to be cured, against which creatures does she cause the river to rise to stop their pursuit of her and Frodo?"
-console.log(question1)
+// GET DOM ELEMENTS
+const innerContainer = document.getElementById("inner-container");
+const scoreControl = document.getElementById("score-control");
+const numberCorrect = document.getElementById("number-correct");
+const gc1 = document.getElementById("gc1");
+const questionNumber = document.getElementById("question-number");
+const startGameMsg = document.getElementById("start-game-msg")
+const winGameMsg = document.getElementById("win-game-msg")
+const gc3 = document.getElementById("gc3");
+const question = document.getElementById("question")
+const gc4 = document.getElementById("gc4");
+const gc5 = document.getElementById("gc5");
+const gc6 = document.getElementById("gc6");
+const startBtn = document.getElementById("start-btn");
 
+
+// CONVERT ANSWER-REFS NODELIST TO AN ARRAY
+const answerControlNodes = document.querySelectorAll(".answer-control");
+let answerControls = Array.from(answerControlNodes);
+
+// CONVERT ANSWER-REFS NODELIST TO AN ARRAY
+const answerRefNodes = document.querySelectorAll(".answerRef");
+let answerRefs = Array.from(answerRefNodes);
+
+// CONVERT 'ANSWER' NODELIST TO AN ARRAY
+const answerNodeList = document.querySelectorAll(".answer");
+let answers = Array.from(answerNodeList);
+
+
+// GET ALL GAME ELEMENTS (FOR WIN CONDITION)
+const gameElements = [gc1, gc2, gc3, gc4, answers[0], answers[1], answers[2], answers[3], answerRefs[0], answerRefs[1], answerRefs[2], answerRefs[3]];
+
+
+//ASSIGN ALL THE GAME QUESTIONS TO VARIABLES
+const question1 = "After Frodo was wounded on Weathertop, Arwen takes  him to Rivendell to be healed. Along the way, against which creatures does she cause the river to rise to stop their pursuit of her and Frodo?"
 const question2 = "How many Rings of Power did Sauron make?"
-console.log(question2)
-
 const question3 = "In the Fellowship of the Ring, what does Galadriel give to Frodo before the Fellowship departs?"
-console.log(question3)
-
 const question4 = "Throughout all three movies, how many times total does Frodo put on the One Ring?"
-console.log(question4)
-
 const question5 = "Which hobbit offers himself in service to Denethor, Steward of Gondor?"
-console.log(question5)
-
 const question6 = "What is (or what are) the Palantir?"
-console.log(question6)
-
 const question7 = "In which country was the entire movie trilogy filmed?"
-console.log(question7)
-
 
 
 //SETS ALL ANSWER CHOICES FOR EACH QUESTION TO AN ARRAY 
 const q1AnswerSet = ["Maiar", "Nazgul", "Uruk-hai", "Orcs"]
-console.log(q1AnswerSet)
-
 const q2AnswerSet = ["7", "25", "20", "9"]
-console.log(q2AnswerSet)
-
-const q3AnswerSet = ["Mithril shirt", "A sword", "The Light of Earendil", "A golden brooch"]
-console.log(q3AnswerSet)
-
+const q3AnswerSet = ["Mithril shirt", "The Light of Earendil", "A sword", "A golden brooch"]
 const q4AnswerSet = ["3", "5", "2", "4"]
-console.log(q4AnswerSet)
-
 const q5AnswerSet = ["Bilbo", "Pippin", "Merry", "Frodo"]
-console.log(q5AnswerSet)
-
 const q6AnswerSet = ["One of the lost seeing-stones", "Aragorn's sword", "The bridge in the mines of Moria", "An offshoot race of the Elves"]
-console.log(q6AnswerSet)
-
 const q7AnswerSet = ["Denmark", "Finland", "England", "New Zealand"]
-console.log(q7AnswerSet)
 
 
-//SELECTS AND STORES VARIOUS ELEMENTS TO VARIABLES
-const startBtn = document.querySelector('#startBtn')
-console.log(startBtn)
+// SCORING MECHANISM
+let score = 0;
+let correct;
+function incrementScore () {
+  score++;
+  numberCorrect.innerHTML = score.toString();
+}
+  
 
+//SETS ALL THE TEXT FOR EACH QUESTION AND ANSWER SET
+function displayQuestion(qNumber, q, answerSet) {
+    questionNumber.innerHTML = `QUESTION ${qNumber.toString().toUpperCase()}`;
+    gc1.classList.remove('hidden');
+    question.innerHTML = q;
+    gc3.classList.remove('hidden');
+    gc4.classList.remove('hidden');
 
-const questionNumber = document.querySelector('#questionNumber')
-console.log(questionNumber)
+    answers.forEach((item, index) => {
+      item.classList.remove('hidden');
+      item.innerHTML = answerSet[index];
+    })
 
-const startGameMsg = document.querySelector('#startGameMsg')
+    answerRefs.forEach((item) => {
+      item.classList.remove('hidden');
+    })
 
-const question = document.querySelector('#question')
-console.log(question)
-
-const choice1 = document.querySelector('#answer1')
-const choice2 = document.querySelector('#answer2')
-const choice3 = document.querySelector('#answer3')
-const choice4 = document.querySelector('#answer4')
-
-const answerFeedback = document.querySelector('#answerFeedback')
-
-
-//STORES ALL ANSWER SET VARIABLES IN AN ARRAY SO THEY CAN BE LOOPED OVER LATER
-const answerArray = [choice1, choice2, choice3, choice4]
-
-
-
-//VARIOUS FUNCTION DECLARATIONS TO BE USED IN THE GAME LOGIC
-const incorrectAnswer = () => {
-  answerFeedback.innerHTML = "INCORRECT... TRY AGAIN"
-
-  setTimeout( () => {
-    answerFeedback.innerHTML = ""
-  }, 2000)
 }
 
-const correctAnswer1 = () => {
-  answerFeedback.innerHTML = "CORRECT!"
-  
-  setTimeout( () => {
-    answerFeedback.innerHTML = ""
-    secondQuestion()
-  }, 2000)
-  
+
+// GO TO NEXT QUESTION
+const nextQuestion = () => {
+  switch (questionNumber.innerHTML) {
+    case "QUESTION ONE":
+      secondQuestion();
+      break;
+    case "QUESTION TWO":
+      thirdQuestion();
+      break;
+    case "QUESTION THREE":
+      fourthQuestion();
+      break;
+    case "QUESTION FOUR":
+      fifthQuestion();
+      break;
+    case "QUESTION FIVE":
+      sixthQuestion();
+      break;
+    case "QUESTION SIX":
+      seventhQuestion();
+      break;
+    case "QUESTION SEVEN":
+      scoreScreen();
+      break;
+  }
 }
 
-const correctAnswer2 = () => {
-  answerFeedback.innerHTML = "CORRECT!"
-  
-  setTimeout( () => {
-    answerFeedback.innerHTML = ""
-    thirdQuestion()
-  }, 2000)
+
+// HANDLE SELECTED ANSWER
+const checkAnswer = (evt) => {
+  if (evt.target === correct) {
+      evt.target.classList.add('selected');
+      evt.target.innerHTML = "CORRECT!"
+      incrementScore();
+      setTimeout(() => {
+        evt.target.classList.remove('selected');
+        resetAnswers()
+        nextQuestion();
+      }, 2500)
+  } else {
+      evt.target.classList.add('selected');
+      evt.target.innerHTML = "INCORRECT!";
+      setTimeout(() => {
+        evt.target.classList.remove('selected');
+        resetAnswers()
+        nextQuestion();
+      }, 2500)
+  }
 }
 
-const correctAnswer3 = () => {
-  answerFeedback.innerHTML = "CORRECT!"
-  
-  setTimeout( () => {
-    answerFeedback.innerHTML = ""
-    fourthQuestion()
-  }, 2000)
+
+// ATTACH EVENT LISTENERS TO ANSWER CHOICES
+function handleSelection () {
+    answers[0].addEventListener('click', checkAnswer);
+    answers[1].addEventListener('click', checkAnswer);
+    answers[2].addEventListener('click', checkAnswer);
+    answers[3].addEventListener('click', checkAnswer);
 }
 
-const correctAnswer4 = () => {
-  answerFeedback.innerHTML = "CORRECT!"
-  
-  setTimeout( () => {
-    answerFeedback.innerHTML = ""
-    fifthQuestion()
-  }, 2000)
- 
+
+// RESET ANSWERS (REMOVE ANSWER-FEEDBACK CLASS & EVENT LISTENERS)
+function resetAnswers () {
+  answers[0].removeEventListener('click', checkAnswer);
+  answers[1].removeEventListener('click', checkAnswer);
+  answers[2].removeEventListener('click', checkAnswer);
+  answers[3].removeEventListener('click', checkAnswer);
 }
 
-const correctAnswer5 = () => {
-  answerFeedback.innerHTML = "CORRECT!"
-  
-  setTimeout( () => {
-    answerFeedback.innerHTML = ""
-    sixthQuestion()
-  }, 2000)
-}
 
-const correctAnswer6 = () => {
-  answerFeedback.innerHTML = "CORRECT!"
-  
-  setTimeout( () => {
-    answerFeedback.innerHTML = ""
-    seventhQuestion()
-  }, 2000)
-}
-
-const correctAnswer7 = () => {
-  answerFeedback.innerHTML = "CONGRATULATIONS, YOU GOT ALL THE CORRECT ANSWERS!"
-  
-  setTimeout( () => {
-    resetGame()
-  }, 3000)
-}
-
+// RESET GAME TO STARTING SCREEN
 const resetGame = () => {
   window.location.reload()
 }
 
+
+// FUNCTION TO START GAME
 const beginGame = () => {
+  startGameMsg.classList.add('hidden');
   firstQuestion()
 }
+  
 
-
-//BEGINS THE GAME WHEN THE START BUTTON IS CLICKED
+// INITIALIZE GAME WHEN THE START BUTTON IS CLICKED
 startBtn.addEventListener('click', beginGame)
-
 
 
 //FUNCTIONS FOR EACH QUESTION
 function firstQuestion(){
-    //ADDS THE HOVER CLASS TO ALL THE ANSWER DIVS
-    answerArray.map((item) => {
-      item.classList.add('hover')
+    correct = answers[1];
+
+    //CHANGE INNERHTML AND FUNCTIONALITY OF STARTBTN DURING GAMEPLAY
+    startBtn.removeEventListener('click', beginGame);
+    startBtn.classList.remove('me-font-sm');
+    startBtn.classList.add('me-font-xs');
+    startBtn.innerHTML = "RESET GAME";
+    startBtn.style.marginTop = "3rem";
+    startBtn.addEventListener('click', resetGame);
+    scoreControl.classList.remove('hidden');
+
+    displayQuestion('one', question1, q1AnswerSet);
+
+    answerControls.forEach((item) => {
+      item.addEventListener('mouseover', (e) => {
+        item.style.cursor = "pointer";
+        item.children[0].classList.add('highlight');
+        item.children[1].classList.add('highlight');
+      })
+      item.addEventListener('mouseout', (e) => {
+        item.style.cursor = "default";
+        item.children[0].classList.remove('highlight');
+        item.children[1].classList.remove('highlight');
+      })
     })
 
-    //SETS ALL THE TEXT FOR QUESTION ONE
-    questionNumber.innerHTML = "QUESTION ONE:"
-    startGameMsg.innerHTML = ""
-    question.innerHTML = question1
-    choice1.innerHTML = q1AnswerSet[0]
-    choice2.innerHTML = q1AnswerSet[1]
-    choice3.innerHTML = q1AnswerSet[2]
-    choice4.innerHTML = q1AnswerSet[3]
-
-    //CHANGES INNERHTML AND FUNCTIONALITY OF STARTBTN DURING GAMEPLAY
-    startBtn.removeEventListener('click', beginGame)
-    startBtn.innerHTML = "RESET GAME"
-    startBtn.addEventListener('click', resetGame)
-
-    //EVENT LISTENERS FOR THIS ANSWER SET:
-    choice1.addEventListener('click', incorrectAnswer)
-    choice2.addEventListener('click', correctAnswer1)
-    choice3.addEventListener('click', incorrectAnswer)
-    choice4.addEventListener('click', incorrectAnswer)
+    handleSelection();
 }
 
 function secondQuestion(){
-
-  //SETS ALL THE TEXT FOR QUESTION TWO
-  questionNumber.innerHTML = "QUESTION TWO:"
-  question.innerHTML = question2
-  choice1.innerHTML = q2AnswerSet[0]
-  choice2.innerHTML = q2AnswerSet[1]
-  choice3.innerHTML = q2AnswerSet[2]
-  choice4.innerHTML = q2AnswerSet[3]
-
-  //REMOVES EVENT LISTENERS FROM PREVIOUS QUESTION
-  choice2.removeEventListener('click', correctAnswer1)
-  choice3.removeEventListener('click', incorrectAnswer)
-  
-  //NEW EVENT LISTENERS FOR THIS ANSWER SET:
-  choice2.addEventListener('click', incorrectAnswer)
-  choice3.addEventListener('click', correctAnswer2)
+    correct = answers[2];
+    displayQuestion('two', question2, q2AnswerSet);
+    handleSelection();
 }
 
 
 function thirdQuestion(){
-  //TWEAKS SOME STYLES ON THE ANSWER SET, FOR AESTHETIC PURPOSES
-  answerArray.map((item) => {
-    // item.style.width = "10rem";
-    item.style.height = "4rem"
-    item.style.width = "15rem"
-    item.style.fontSize = "1.3rem"
-    item.style.padding = ".5rem"
-    item.style.textAlign = "center"
-
-  })
-
-  //REMOVES RELEVANT EVENT LISTENERS FROM PREVIOUS QUESTION
-  choice3.removeEventListener('click', correctAnswer2)
-
-  //SETS THE TEXT FOR THIS QUESTION AND ANSWER SET
-  questionNumber.innerHTML = "QUESTION THREE:"
-  question.innerHTML = question3
-  choice1.innerHTML = q3AnswerSet[0]
-  choice2.innerHTML = q3AnswerSet[1]
-  choice3.innerHTML = q3AnswerSet[2]
-  choice4.innerHTML = q3AnswerSet[3]
-
-  //NEW EVENT LISTENER:
-  choice3.addEventListener('click', correctAnswer3)
+    correct = answers[1];
+    displayQuestion('three', question3, q3AnswerSet);
+    handleSelection();
 }
 
 
 function fourthQuestion(){
-  
-  //RESETS/MODIFIES TWEAKED STYLES FROM PREVIOUS QUESTION
-  answerArray.map((item) => {
-    item.style.width = "7rem";
-    item.style.height = "3.5rem"
-    item.style.fontSize = "1.4rem"
-    item.style.padding = ".5rem"
-    item.style.textAlign = "center"
-  })
-
-  //SETS THE TEXT FOR THIS QUESTION AND ANSWER SET
-  questionNumber.innerHTML = "QUESTION FOUR:"
-  question.innerHTML = question4
-  choice1.innerHTML = q4AnswerSet[0]
-  choice2.innerHTML = q4AnswerSet[1]
-  choice3.innerHTML = q4AnswerSet[2]
-  choice4.innerHTML = q4AnswerSet[3]
-
-  //REMOVES RELEVANT EVENT LISTENERS FROM PREVIOUS QUESTION
-  choice3.removeEventListener('click', correctAnswer3)
-  choice4.removeEventListener('click', incorrectAnswer)
-
-  //NEW EVENT LISTENERS:
-  choice3.addEventListener('click', incorrectAnswer)
-  choice4.addEventListener('click', correctAnswer4)
+    correct = answers[3];
+    displayQuestion('four', question4, q4AnswerSet);
+    handleSelection();
 }
 
 
 function fifthQuestion(){
-  
-  //SETS THE TEXT FOR THIS QUESTION AND ANSWER SET
-  questionNumber.innerHTML = "QUESTION FIVE:"
-  question.innerHTML = question5
-  choice1.innerHTML = q5AnswerSet[0]
-  choice2.innerHTML = q5AnswerSet[1]
-  choice3.innerHTML = q5AnswerSet[2]
-  choice4.innerHTML = q5AnswerSet[3]
-
-  //REMOVES RELEVANT EVENT LISTENERS FROM PREVIOUS QUESTION
-  choice2.removeEventListener('click', incorrectAnswer)
-  choice4.removeEventListener('click', correctAnswer4)
-  
-  //NEW EVENT LISTENERS:
-  choice2.addEventListener('click', correctAnswer5)
-  choice4.addEventListener('click', incorrectAnswer)
+    correct = answers[1];
+    displayQuestion('five', question5, q5AnswerSet);
+    handleSelection();
 }
 
 
 function sixthQuestion(){
-  
-  //TWEAKS SOME STYLES ON THE ANSWER SET, FOR AESTHETIC PURPOSES
-  answerArray.map((item) => {
-    item.style.width = "10rem";
-    item.style.height = "4.3rem"
-    item.style.width = "15rem"
-    item.style.fontSize = "1.2rem"
-    item.style.padding = ".5rem"
-    item.style.textAlign = "center"
-  })
-
-  //SETS THE TEXT FOR THIS QUESTION AND ANSWER SET
-  questionNumber.innerHTML = "QUESTION SIX:"
-  question.innerHTML = question6
-  choice1.innerHTML = q6AnswerSet[0]
-  choice2.innerHTML = q6AnswerSet[1]
-  choice3.innerHTML = q6AnswerSet[2]
-  choice4.innerHTML = q6AnswerSet[3]
-
-  //REMOVES RELEVANT EVENT LISTENERS FROM PREVIOUS QUESTION
-  choice1.removeEventListener('click', incorrectAnswer)
-  choice2.removeEventListener('click', correctAnswer5)
-
-  //NEW EVENT LISTENERS:
-  choice1.addEventListener('click', correctAnswer6)
-  choice2.addEventListener('click', incorrectAnswer)
+    correct = answers[0];
+    displayQuestion('six', question6, q6AnswerSet);
+    handleSelection();
 }
 
 
 function seventhQuestion(){
-  //RESETS/MODIFIES TWEAKED STYLES FROM PREVIOUS QUESTION
-  answerArray.map((item) => {
-    item.style.width = "10rem";
-    item.style.height = "3.5rem"
-    item.style.fontSize = "1.3rem"
-    item.style.padding = ".5rem"
-    item.style.textAlign = "center"
-  })
-
-  //SETS THE TEXT FOR THIS QUESTION AND ANSWER SET
-  questionNumber.innerHTML = "QUESTION SEVEN:"
-  question.innerHTML = question7
-  choice1.innerHTML = q7AnswerSet[0]
-  choice2.innerHTML = q7AnswerSet[1]
-  choice3.innerHTML = q7AnswerSet[2]
-  choice4.innerHTML = q7AnswerSet[3]
-
-  //REMOVES RELEVANT EVENT LISTENERS FROM PREVIOUS QUESTION
-  choice1.removeEventListener('click', correctAnswer6)
-  choice4.removeEventListener('click', incorrectAnswer)
-
-  //NEW EVENT LISTENERS:
-  choice1.addEventListener('click', incorrectAnswer)
-  choice4.addEventListener('click', correctAnswer7)
+    correct = answers[3];
+    displayQuestion('seven', question7, q7AnswerSet);
+    handleSelection();
 }
+
+
+function scoreScreen() {
+    innerContainer.classList.remove('flexcolumn')
+    innerContainer.style.display = "flex";
+    innerContainer.style.flexDirection = "column";
+    innerContainer.style.justifyContent = "center";
+    startBtn.classList.add('hidden');
+
+    gameElements.forEach((element) => {
+        element.classList.add('hidden');
+    });
+
+    winGameMsg.style.color = '#fcdd66';
+    if (score >= 4){
+      winGameMsg.innerHTML = "CONGRATULATIONS, YOU WIN!"
+    } else {
+      winGameMsg.innerHTML = `SORRY... YOU ONLY GOT ${score} CORRECT ANSWERS. TRY AGAIN!`
+    }
+    gc5.classList.remove('hidden');
+
+    setTimeout(() => {
+        resetGame();
+    }, 3000);
+}
+
